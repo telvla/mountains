5441209e44467702631eb3b3234fd59dd72b8b8d
Telvla <telvla.trifonov@gmail.com>
2022-02-13 20:37:30 +0300

    Upload project

---
+++ public/js/app.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    body_height_screen = document.documentElement.clientHeight;
+
+    $(".header-left").css('height', body_height_screen);
+    $(".header-right").css('height', body_height_screen);
+    $(".first-screen").css('height', body_height_screen);
+
+    $(".wr-first-screen-select").css('margin-top', (body_height_screen / 2) - 100);
+
+
+    $(".line-1").css('margin-top', (body_height_screen / 2) - 90);
+
+    $(".left-icon").css('margin-left', '3%');
+    $(".left-icon-1").css('margin-top', (body_height_screen / 2) - 50);
+    $(".left-icon-2").css('margin-top', (body_height_screen / 2) - 100);
+    $(".left-icon-3").css('margin-top', (body_height_screen / 2) - 150);
+});
+
