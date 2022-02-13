0c76039aa8818e0de431e1b7ad0ba1144ecc9c78
Telvla <telvla.trifonov@gmail.com>
2022-02-13 22:57:45 +0300

    created template

---
+++ resources/views/header.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Домбай</title>
+
+        <base href="">
+
+        <link rel="stylesheet" href="/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="/css/font-awesome/css/font-awesome.css" />
+        <link rel="stylesheet" href="/css/style.css" />
+    </head>
+    <body>
+
+       
