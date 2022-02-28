35b54173af7814f7a6fd639495d4c912cb1d1321
Telvla <telvla.trifonov@gmail.com>
2022-02-28 23:08:50 +0300

    add tempalate meta property

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -8,6 +8,17 @@
 
         <base href="{{ env('APP_URL') }}">
 
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+
+        <meta property="og:image" content="">
+        <meta property="og:image:width" content="">
+        <meta property="og:image:height" content="">
+        <meta property="og:image:type" content="image/jpeg">
+        <meta property="og:title" content="">
+        <meta property="og:description" content="">
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="{{ env('APP_URL') }}">        
+
         <link rel="stylesheet" href="/css/bootstrap.min.css" />
         <link rel="stylesheet" href="/css/font-awesome/css/font-awesome.css" />
         <link rel="stylesheet" href="/css/style.css" />
