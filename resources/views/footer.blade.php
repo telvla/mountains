fcda7a20e6a28e9c85600acbb8533a1c9337068a
Telvla <telvla.trifonov@gmail.com>
2022-02-28 23:24:43 +0300

    edit template include js

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -168,7 +168,7 @@
             </div>
         </footer>
 
-        <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+        <script type="text/javascript" src="js/jquery-3.6.0.min.js"></script>
         <script type="text/javascript" src="js/bootstrap.js"></script>
         <script type="text/javascript" src="js/app.js"></script>
         {{-- include page js file --}}
