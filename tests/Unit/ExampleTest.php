c98452cb7c745eb95d93c91c53ce2afb15f56892
Telvla <telvla.trifonov@gmail.com>
2022-03-26 22:06:31 +0300

    add test

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -19,3 +19,78 @@ class ExampleTest extends TestCase
         $response->assertSee('11111');
     }
 }
+
+
+
+/* 
+Проверка страницы на доступность
+ 
+namespace Tests\Unit;
+ 
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+ 
+class ExampleTest extends TestCase
+{
+    public function testBasicTest()
+    {
+        $response = $this->get('/test');
+        $response->assertStatus(200);        
+    }
+}
+*/ 
+ 
+/* 
+Проверка страницы на значение "текст"
+ 
+namespace Tests\Unit;
+ 
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+ 
+class ExampleTest extends TestCase
+{
+    public function testBasicTest()
+    {
+        $response = $this->get('/test');
+        $response->assertSee('11111');
+    }
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
